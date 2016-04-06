e1861bad9fcb2d36064db61ae31ecc656385b788
Robert Yang <yang.robert.w@gmail.com>
2016-04-06 00:56:09 -0400

    week10: ListDestroyDemo, LayerMaskDemo (just misc stuff that came up from homework)

---
+++ week10/Assets/scripts/LayerMaskDemo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LayerMaskDemo : MonoBehaviour {
+
+	public LayerMask raycastMask; // assign in Inspector
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+		RaycastHit rayHitInfo = new RaycastHit();
+
+		if ( Physics.Raycast( ray, out rayHitInfo, 1000f, raycastMask ) ) {
+			rayHitInfo.transform.localScale *= 1.01f; // make WHATEVER grow bigger
+		}
+	}
+
+}
---
+++ week10/Assets/scripts/ListDestroyDemo.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ListDestroyDemo : MonoBehaviour {
+
+	public List<string> listOfFailingStudents = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+		listOfFailingStudents.Add("Noah");
+		listOfFailingStudents.Add("Elijah");
+		listOfFailingStudents.Add("Elijah");
+		listOfFailingStudents.Add("Elijah");
+		listOfFailingStudents.Add("Elijah");
+		listOfFailingStudents.Add("Elijah");
+		listOfFailingStudents.Add("Elijah");
+		listOfFailingStudents.Add("Elijah");
+		listOfFailingStudents.Add("Erik");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// print every item out to the console
+		if ( Input.GetKeyDown(KeyCode.Space) ) {
+			foreach ( string studentName in listOfFailingStudents ) {
+				Debug.Log(studentName);
+			}
+		}
+
+		// a bit trickier: how to remove certain list items with foreach
+		if ( Input.GetKeyDown(KeyCode.A) ) {
+			List<string> successfulStudents = new List<string>();
+
+			foreach ( string studentName in listOfFailingStudents ) {
+				if ( studentName.StartsWith("E") ) {
+					// you cannot remove items directly from a list while we are foreach'ing
+					// the line of code below will give you an error
+					// listOfFailingStudents.Remove( studentName );
+
+					// instead, make a list of items to remove...
+					successfulStudents.Add( studentName );
+				}
+			}
+
+			// ... and then in a separate foreach loop, remove them
+			foreach ( string goodStudent in successfulStudents ) {
+				listOfFailingStudents.Remove(goodStudent);
+			}
+		}
+
+	}
+}
